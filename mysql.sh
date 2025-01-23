d1029e8295935c2ab0d43e473ce352bd6fdefb17
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-23 22:32:16 +0530

    added

---
+++ mysql.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 1
+    else
+        echo "$2 is $G success $N" 
+    fi
+}
+
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR :: Please run this script with sudo access $N"
+        exit 1
+    fi
+
+}
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Mysql server..."
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Mysql server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Start Mysql server"
+
+mysql -h sql.relationhospital.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" 
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "MySQL Root password already setup ... $Y SKIPPING $N
+
+fi
