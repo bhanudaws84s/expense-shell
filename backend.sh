f5ffebc93d539697f2a7ad5b60e398179b1101ec
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-23 23:21:04 +0530

    added

--- backend.sh
+++ backend.sh
@@ -61,6 +61,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading the backend code"
 
 cd /app &>>$LOG_FILE_NAME
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Unzipping the code"
